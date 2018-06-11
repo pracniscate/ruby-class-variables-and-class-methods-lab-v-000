cc11ff70b6ac09a5ca582333993af7db73189315
Lorena <lorena.giddens@outlook.com>
2018-06-11 11:31:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,9 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] 
-        genre_count[genre] +=1 
-      else 
+      if genre_count[genre]
+        genre_count[genre] +=1
+      else
         genre_count[genre] = 1
       end
     end
